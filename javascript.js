b07058f8be525b5cf0952ea8d97ed2b66480b93a
Raunak1089 <76219402+Raunak1089@users.noreply.github.com>
2024-10-27 11:54:43 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+let music1 = new Audio(
+      "https://raunak1089.github.io/Required-files/GunShot.mp3"
+    );
+let music2 = new Audio(
+  "https://raunak1089.github.io/Required-files/CymbalCrash.mp3"
+);
+
 
 if (window.innerHeight>window.innerWidth){
   styles = `
@@ -16,25 +23,64 @@ if (window.innerHeight>window.innerWidth){
 }
 
 function refresh() {
-    if (!document.fullscreenElement) {
-      location.href=location.href;
-    }
+  if (!document.fullscreenElement) {
+    location.href=location.href;
   }
+}
 
 window.addEventListener('resize', refresh);
 
 
 function play_audio(link){
-  var put = document.createElement('audio');
-  put.src = 'https://raunak1089.github.io/Required-files/' + link; 
-  document.body.appendChild(put);
-  put.play();
-  setTimeout(()=>{
-    setTimeout(()=>{put.remove();}, put.duration*1000);
-  }, 500);
+    var put = document.createElement('audio');
+    put.src = 'https://raunak1089.github.io/Required-files/' + link; 
+    document.body.appendChild(put);
+    put.play();
+    setTimeout(()=>{
+      setTimeout(()=>{put.remove();}, put.duration*1000);
+    }, 500);
 }
 
-let fire;
+function fire() {
+    started = true;
+    play_audio('GunShot.mp3'); 
+
+    // CREATE BULLET
+    bullet = document.createElement("div"); bullet.id='bullet';
+    bullet.style.marginTop = gun.getBoundingClientRect().y+'px';
+    document.getElementById("demo").appendChild(bullet);
+    setTimeout(()=>{bullet.remove(); }, 500);
+
+
+    gun.classList.add("gunanm");
+    setTimeout(()=>{
+    gun.classList.remove("gunanm");
+    },300);
+
+
+
+    document.getElementById("gun").removeEventListener('click', fire);
+    window.removeEventListener('keydown',fire_with_key)
+
+    document.getElementById("dialogue").innerText = "Loading...";
+    setTimeout(()=>{
+      document.getElementById("gun").addEventListener('click', fire);
+      window.addEventListener('keydown',fire_with_key)
+
+      document.getElementById("dialogue").innerText = "Shoot!";
+    }, 1000);
+
+    // MAKE COUNTED VARIABLE FALSE TO ENABLE SCORE COUNTING
+    setTimeout(()=>{
+      counted = false;
+    }, 500);
+
+
+    // console.log(gun.getBoundingClientRect());
+
+};
+
+
 function fire_with_key(event){
     if (event.key.toLowerCase() === 'd'){
         fire();
@@ -51,54 +97,16 @@ function startGame() {
     var score2 = document.getElementById("score2");
 
     document.getElementById("gun").addEventListener('click', fire);
+    
     window.addEventListener('keydown',fire_with_key)
 
     // START BALL DROPPING
     ball.classList.add("dropanim");
 
-    fire = () => {
-      started = true;
-      play_audio('GunShot.mp3'); 
-
-      // CREATE BULLET
-      bullet = document.createElement("div"); bullet.id='bullet';
-      bullet.style.marginTop = gun.getBoundingClientRect().y+'px';
-      document.getElementById("demo").appendChild(bullet);
-      setTimeout(()=>{bullet.remove(); }, 500);
 
 
-            gun.classList.add("gunanm");
-            setTimeout(()=>{
-            gun.classList.remove("gunanm");
-            },300);
-
-
-
-          document.getElementById("gun").removeEventListener('click', fire);
-          window.removeEventListener('keydown',fire_with_key)
-
-          document.getElementById("dialogue").innerText = "Loading...";
-        setTimeout(()=>{
-          document.getElementById("gun").addEventListener('click', fire);
-          window.addEventListener('keydown',fire_with_key)
-
-          document.getElementById("dialogue").innerText = "Shoot!";
-        }, 1000);
-
-        // MAKE COUNTED VARIABLE FALSE TO ENABLE SCORE COUNTING
-        setTimeout(()=>{
-          counted = false;
-        }, 500);
-
-
-        // console.log(gun.getBoundingClientRect());
-
-
-    };
-
-
-            // if(ball.getBoundingClientRect().y>270){play_audio('Balldrop.mp3');}
-            // if(ballTop=145){drop1.pause(); drop1.currentTime = 0;}
+    // if(ball.getBoundingClientRect().y>270){play_audio('Balldrop.mp3');}
+    // if(ballTop=145){drop1.pause(); drop1.currentTime = 0;}
 
 
     setInterval(function() {
